bdcd47e4672da7202be3c59a2a4a8051d1ded394
Sadiq Ali <142330669+theSadiqali@users.noreply.github.com>
2025-12-06 02:40:54 +0100

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar.jsx";
 import KnowledgeCard from "./components/KnowledgeCard";
 import Chatbot from "./components/Chatbot";
 
